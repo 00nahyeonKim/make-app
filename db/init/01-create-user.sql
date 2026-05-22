0d2d211f807b93c50fa867ec7a756c806436b438
nahyeon <nahyeon000918@gmail.com>
2026-05-22 20:42:45 +0900

    .env 템플릿 제공

--- db/init/01-create-user.sql
+++
@@ -1,5 +0,0 @@
--- db/init/01-create-user.sql
--- gvenzl 이미지의 APP_USER 환경변수가 이미 처리해주므로 보통 비워둬도 됨
--- 추가 권한이 필요하면 여기에:
-ALTER SESSION SET CONTAINER = XEPDB1;
-GRANT CREATE TABLE, CREATE SEQUENCE, CREATE VIEW TO makeapp;
