13d186dd96005d730fd3be241f3d962bff064b0a
00nahyeonKim <nahyeon000918@gmail.com>
2026-05-27 20:27:00 +0900

    카카오 developer jwt 등록 완료

--- backend/src/main/java/com/makeapp/backend/BackendApplication.java
+++ backend/src/main/java/com/makeapp/backend/BackendApplication.java
@@ -2,8 +2,11 @@ package com.makeapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+// JPA Auditing 기능을 활성화해서, 엔티티 생성/수정 시간을 자동으로 기록
+@EnableJpaAuditing
 public class BackendApplication {
 
 	public static void main(String[] args) {
